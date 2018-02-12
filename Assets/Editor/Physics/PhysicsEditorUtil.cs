d3619c1a2ab13a4069620e0c5ad8ce9de33c8ddc
ftvoid <gensaicuttlefish@gmail.com>
2018-02-12 12:42:42 +0900

    Layer Collision Matrixの設定を一括でOFFする機能を追加。

---
+++ Assets/Editor/Physics/PhysicsEditorUtil.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class PhysicsEditorUtil {
+    [MenuItem("UnityEditorUtil/Physics/Clear All Layer Collision Matrix")]
+    public static void ClearAllLayerCollisionMatrix() {
+        const string Path = "ProjectSettings/DynamicsManager.asset";
+
+        var serialized = new SerializedObject(AssetDatabase.LoadAssetAtPath<Object>(Path));
+        if ( serialized == null ) {
+            return;
+        }
+
+        var matrix = serialized.FindProperty("m_LayerCollisionMatrix");
+        if ( matrix == null ) {
+            return;
+        }
+
+        if ( !matrix.isArray ) {
+            return;
+        }
+
+        for ( int i = 0 ; i < matrix.arraySize ; ++i ) {
+            var value = matrix.GetArrayElementAtIndex(i);
+            value.intValue = 0;
+        }
+
+        serialized.ApplyModifiedProperties();
+
+        AssetDatabase.SaveAssets();
+    }
+}
